67efb90199a132fb041ddf55ef72b6e88aa2190a
Abhishek15016 <abhishek.yadav.codes@gmail.com>
2026-02-16 21:49:04 +0530

    note

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 import config
+import models
 import routes
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,43 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    passhash = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(64), nullable=True)
+    isadmin = db.Column(db.Boolean,nullable=False, default=False)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    products = db.relationship('Product', backref='category', lazy=True) 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    man_date = db.Column(db.Boolean, nullable=False)
+    carts = db.relationship('cart', backref='product', lazy=True)
+
+class cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    transactiom_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
